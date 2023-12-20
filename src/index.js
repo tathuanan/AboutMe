01def4d827de8cda00879c68b32f5e2de144c022
Murillo Santos <murillo-santos@outlook.com.br>
2023-12-20 20:02:04 -0300

    Criação do Header, inclusão de navbar, inclusão de imagem ao header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HeaderNavBar from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HeaderNavBar/>
   </React.StrictMode>
 );
 
