9a16629208b56f214b67566cb2a562b307c6bc70
Murillo Santos <murillo-santos@outlook.com.br>
2023-12-20 20:05:26 -0300

    Inclusão das Pastas Assets e Components

---
+++ src/components/Images.js
@@ -0,0 +1,15 @@
+import HeaderImg from '../assets/MySelf.jpg'
+import '../Styles.css';
+
+const MySelf = () => {
+    return(
+        <div>
+            <img
+            src={HeaderImg}
+            className='App-HeaderImg'
+            />
+        </div>
+    )
+}
+
+export default MySelf
